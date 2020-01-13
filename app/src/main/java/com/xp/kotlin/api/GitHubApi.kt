ddb2a67074a0eea7db1c8aa9eef27b53ccbe401b
xp664221812 <realxp141414@163.com>
2020-01-13 18:04:40 +0800

    add

---
+++ app/src/main/java/com/xp/kotlin/api/GitHubApi.kt
@@ -0,0 +1,11 @@
+package com.xp.kotlin.api
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface GitHubApi {
+
+    @GET("/repos/{owner}/{repo}")
+    fun getRepository(@Path("owner")owner:String,@Path("repo")repo:String ):Call<>
+}
